92ffb4d177adf57a6bb7a27b3124c235a4558dbb
Oleksii Andriienko <alex.v.andrienko@gmail.com>
2019-01-02 18:35:35 +0200

    first commit

---
+++ src/main/java/ua/com/sourceit/andriienko/Tourist_agency/App.java
@@ -0,0 +1,13 @@
+package ua.com.sourceit.andriienko.Tourist_agency;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
