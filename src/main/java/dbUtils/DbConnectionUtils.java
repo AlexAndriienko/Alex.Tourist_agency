961a55172b5c6bd5f41a7b4b5111605d28a4cf3c
Oleksii Andriienko <alex.v.andrienko@gmail.com>
2019-02-16 01:54:53 +0200

    comm

--- src/main/java/dbUtils/DbConnectionUtils.java
+++ src/main/java/dbUtils/DbConnectionUtils.java
@@ -3,23 +3,23 @@ package dbUtils;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-
 import utils.ReadPropertiesFile;
 
 import java.sql.Connection;
 
 public class DbConnectionUtils {
 	
+	private static final String HOST = "jws-app-mysql";
+	private static final String DB_NAME = "tourist_agency";
+	private static final int PORT  = 3306;
 	private static final String PATH_DB_CONFIGS = "properties/dbConfig.properties";
 	private static final String USERNAME = ReadPropertiesFile.readFile(PATH_DB_CONFIGS, "db.login");
 	private static final String PASSWORD = ReadPropertiesFile.readFile(PATH_DB_CONFIGS, "db.password");
-	private static final String CONNECTIONURL = ReadPropertiesFile.readFile(PATH_DB_CONFIGS, "db.host");
-	private static final String MYSQL_JDBC_DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+	private static final String CONNECTIONURL = "jdbc:mysql://" + HOST + ":" + PORT + "/"+ DB_NAME;
+	private static final String MYSQL_JDBC_DRIVER_NAME = "com.mysql.jdbc.Driver";
 
 	public static Connection getConnection() {
+						
 		try {
 			Class.forName(MYSQL_JDBC_DRIVER_NAME);
 			return DriverManager.getConnection(CONNECTIONURL, USERNAME, PASSWORD);
@@ -29,11 +29,11 @@ public class DbConnectionUtils {
 	}
 	
 	public static Connection getConnectionPool() {
+		
 		try {
-			InitialContext initContext = new InitialContext();
-			DataSource dataSource = (DataSource) initContext.lookup("java:comp/env/jdbc/tourist_agency");
-			return dataSource.getConnection();
-		} catch (NamingException | SQLException e) {
+			Class.forName(MYSQL_JDBC_DRIVER_NAME);
+			return DriverManager.getConnection(CONNECTIONURL, USERNAME, PASSWORD);
+		} catch (ClassNotFoundException | SQLException e) {
 			throw new RuntimeException(e);
 		}
 		
